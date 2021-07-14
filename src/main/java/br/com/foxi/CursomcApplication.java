dec757842303c3351f478de9af49835b5e712595
Alcenir Barbosa dos Santos <alceni_abs@hotmail.com>
2021-07-14 18:18:42 -0300

    criando operacao de instanciacao

--- src/main/java/br/com/foxi/CursomcApplication.java
+++ src/main/java/br/com/foxi/CursomcApplication.java
@@ -1,12 +1,33 @@
 package br.com.foxi;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import br.com.foxi.domain.Categoria;
+import br.com.foxi.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
+	}
+	
+	
 }
